77c510f07b7431f2b66451bb5831ee8357ba77bd
ThomasPruner <thomaspruner@hotmail.com>
2026-05-19 00:47:48 -0300

    feat: add editar-quadra page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
+import EditarQuadra from './pages/EditarQuadra/EditarQuadra'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/"      element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin/editar-quadra/:id" element={<EditarQuadra />} />
       </Routes>
     </BrowserRouter>
   )
